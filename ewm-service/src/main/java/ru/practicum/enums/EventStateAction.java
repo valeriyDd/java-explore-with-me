ac203c7cf9a6b63bc6fb74149bf4b041a6b85666
VaLL <vallall122198@gmail.com>
2023-07-25 10:52:20 +0300

    main_svc

---
+++ ewm-service/src/main/java/ru/practicum/enums/EventStateAction.java
@@ -0,0 +1,28 @@
+package ru.practicum.enums;
+
+import lombok.Getter;
+
+
+@Getter
+public enum EventStateAction {
+    SEND_TO_REVIEW(EventState.PENDING),
+    CANCEL_REVIEW(EventState.CANCELED),
+    PUBLISH_EVENT(EventState.PUBLISHED),
+    REJECT_EVENT(EventState.CANCELED);
+
+    private final EventState eventState;
+
+    EventStateAction(EventState eventState) {
+        this.eventState = eventState;
+    }
+
+
+    public static EventStateAction from(String name) {
+        for (EventStateAction esa : values()) {
+            if (esa.name().equalsIgnoreCase(name)) {
+                return esa;
+            }
+        }
+        throw new IllegalArgumentException("Unknown event state action: " + name);
+    }
+}
